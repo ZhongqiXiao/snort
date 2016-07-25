7024450fd8dc7a19a815fffeef39e452f299edc6
amabrouki <mabrouki.abderrahim@gmail.com>
2016-07-25 09:46:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ service apache2 start
 #/usr/local/bin/barnyard2 -c /etc/snort/barnyard2.conf -d /var/log/snort -f snort.u2 -q -w /var/log/snort/barnyard2.waldo -g snort -u snort -D -a /var/log/snort/archived_logs &
 
 # run snorby
-cd /var/www/html/snorby/ && bundle exec rails server -e production &
+cd /var/www/html/snorby/ && bundle exec rails server -e production
 
