07ca6ed15a1059ec65596f7e6a13eea122695b62
amabrouki <mabrouki.abderrahim@gmail.com>
2016-07-25 09:19:43 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# run mysql et apache2
+service mysql start
+service apache2 start
+
+# run snort
+/usr/local/bin/snort -q -u snort -g snort -c /etc/snort/snort.conf -i
+
+# run barnyard2
+/usr/local/bin/barnyard2 -c /etc/snort/barnyard2.conf -d /var/log/snort -f snort.u2 -q -w /var/log/snort/barnyard2.waldo -g snort -u snort -D -a /var/log/snort/archived_logs
+
